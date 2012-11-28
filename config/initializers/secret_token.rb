9a97e428da2405608c1ef9e0485e916f01330e01
Jack Gao <jackgao2006@gmail.com>
2012-11-28 17:06:49 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giveity::Application.config.secret_token = '7ec8238e3f6df0aa6dab62a37288d668b696460cfb3d4ffa9a858a76ec42935fce67bd66593c872ca9a90b640dce73f0607a92613476e75c9c173bdd262cc57b'
